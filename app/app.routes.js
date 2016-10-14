381cc525ebb97c34642b8070792dfa9190036ba3
Dan Le <dle@geo-jobe.com>
2016-10-14 10:05:28 -0500

    forgot to put in component folder and change html names

--- app/app.routes.js
+++ app/app.routes.js
@@ -8,24 +8,24 @@
         $stateProvider
             .state('about', {
                 url: '/about',
-                templateUrl: '../app/components/about/aboutView.html'
+                templateUrl: '../app/components/about/about.html'
             })
             .state('work', {
                 url: '/work',
-                templateUrl: '../app/components/work/workView.html',
+                templateUrl: '../app/components/work/work.html',
                 controller: 'workController'
             })
             .state('waitr', {
                 url: '/waitr',
-                templateUrl: '../app/components/projects/waitrView.html'
+                templateUrl: '../app/components/projects/waitr.html'
             })
             .state('forage', {
                 url: '/forage',
-                templateUrl: '../app/components/projects/forageView.html'
+                templateUrl: '../app/components/projects/forage.html'
             })
             .state('favour', {
                 url: '/favour',
-                templateUrl: '../app/components/projects/favourView.html'
+                templateUrl: '../app/components/projects/favour.html'
             });
         $urlRouterProvider.otherwise('/work');
     }
